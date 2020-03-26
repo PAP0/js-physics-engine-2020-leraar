c8dda837b8bfadd1cd12e5913198dfa9946498d2
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-03-26 15:07:39 +0100

    commit

---
+++ 02polygons/html/01myPoint/script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+// begin hier met jouw code voor deze opdracht
+
+let colors = ["red","green","blue","orange","yellow","purple"];
+
+function getRandomNumber(max)
+{
+  return Math.floor(Math.random()*max);
+}
+
+function spawn()
+{
+    let myCircle = new Point(getRandomNumber(width), getRandomNumber(height), getRandomNumber(30), colors[getRandomNumber(colors.length)])
+    myCircle.draw();
+}
+
+function animate(){
+    spawn();
+  }
+  
+  setInterval(animate,0)
+
