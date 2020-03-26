b16ed3d58a7ce08f1dbae6e9cc8a2609201f20b1
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-03-26 15:16:39 +0100

    delete

--- 03draggablePoint/lib/MathTools.js
+++
@@ -1,9 +0,0 @@
-/*
-function returning a random number between a min and a maximum value
-*/
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
